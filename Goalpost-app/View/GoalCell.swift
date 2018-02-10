65760c2404497f4e1ef93284df49e8dccec2d3ab
tsoo6464 <tsoo6464@gmail.com>
2018-02-10 17:48:40 +0800

    End of lesson2.

---
+++ Goalpost-app/View/GoalCell.swift
@@ -0,0 +1,25 @@
+//
+//  GoalCell.swift
+//  Goalpost-app
+//
+//  Created by Nan on 2018/2/10.
+//  Copyright © 2018年 nan. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    // Outlets
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+    
+    
+}
