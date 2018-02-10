402b69ca74318aadd427d3d62d72c11c65c73a19
tsoo6464 <tsoo6464@gmail.com>
2018-02-10 16:57:26 +0800

    End of lesson1.

---
+++ Goalpost-app/Controller/GoalsVC.swift
@@ -0,0 +1,26 @@
+//
+//  GoalsVC.swift
+//  Goalpost-app
+//
+//  Created by Nan on 2018/2/10.
+//  Copyright © 2018年 nan. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func addGoalBtnPressed(_ sender: Any) {
+    }
+    
+}
+
