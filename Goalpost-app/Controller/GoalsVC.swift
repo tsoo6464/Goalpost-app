87bc2ab87957c2c4466e40fa176330741d80f8c2
tsoo6464 <tsoo6464@gmail.com>
2018-02-12 20:17:46 +0800

    End of lesson3.

--- Goalpost-app/Controller/GoalsVC.swift
+++ Goalpost-app/Controller/GoalsVC.swift
@@ -12,15 +12,31 @@ class GoalsVC: UIViewController {
 
     // Outlets
     @IBOutlet weak var tableView: UITableView!
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = false
     }
-    
+    // 新增目標
     @IBAction func addGoalBtnPressed(_ sender: Any) {
+        
+    }
+}
+// TableView
+extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else { return UITableViewCell() }
+        cell.configureCell(description: "Eat salad twice a week", type: .shortTrem, goalProgressAmount: 2)
+        return cell
+    }
 }
-
---
+++ Goalpost-app/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  Goalpost-app
+//
+//  Created by Nan on 2018/2/12.
+//  Copyright © 2018年 nan. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTrem = "Short Term"
+}
